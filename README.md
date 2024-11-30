e799b26c20e50937d4eac3a8dfe8027c61560df1
nateneu <ashie.n@northeastern.edu>
2024-11-30 08:58:38 -0800

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+"# Assignment-4---Design-Patterns" 
---
+++ singletonpattern/SilgleTon/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"singletonpattern","l":"Configuration"},{"p":"singletonpattern","l":"SingleObject"},{"p":"singletonpattern","l":"SingletonPatternDemo"},{"p":"singletonpattern","l":"SingletonPatternTest"}];updateSearchResults();
